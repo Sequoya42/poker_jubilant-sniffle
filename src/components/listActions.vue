ec32d962d11aada3ad4cb624ebf70ffca9f20357
Raphael BAUM <rbaum@e1r7p4.42.fr>
2017-09-11 17:20:24 +0200

    forgotten file

---
+++ src/components/listActions.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <v-card v-for="(action, index) in listActions" :key="index">
+  <v-flex>{{action}}</v-flex>
+</v-card>
+</div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: 'listActions',
+
+  data: function() {
+    return {};
+  },
+  computed: {
+    ...mapGetters(['listActions'])
+  },
+  methods: {},
+  components: {}
+};
+</script>
+
+<style>
+
+</style>
